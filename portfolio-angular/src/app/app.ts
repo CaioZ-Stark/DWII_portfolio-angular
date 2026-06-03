35200aac8771a9ac9216f04991dce65beee101fb
CaioZ-Stark <20241ctb0100031@estudantes.ifpr.edu.br>
2026-06-03 03:34:49 +0000

    Teste em Casa de TypeScript e Angular Material

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Meu portfólio-angular');
+  autor = "Caio Mario Zachesky Junior";
 }
