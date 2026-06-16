a2b1bc8c47ea1b5795eb81f26be9fc7a4da8eaab
CaioZ-Stark <20241ctb0100031@estudantes.ifpr.edu.br>
2026-06-16 02:26:43 +0000

    Conceito A

--- api/projetos.php
+++ api/projetos.php
@@ -3,9 +3,31 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
 require __DIR__ . '/../conexao.php';
+/**$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-$sql = "SELECT id, nome, descricao, tecnologias, link_github, ano FROM projetos WHERE  status = 'publicado' ORDER BY ano DESC, id";
-$projetos = $pdo->query($sql)->fetchAll();
+ */
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-echo json_encode($projetos);
+
+
+
+    if($id){
+        $sql = $pdo->prepare("SELECT id, nome, descricao, tecnologias, link_github, ano FROM projetos WHERE id = :id AND status = 'publicado' ORDER BY ano DESC");
+        $sql->execute(['id' => $id]);
+        $projetos = $sql->fetch();
+        if($projetos){
+        echo json_encode($projetos);
+        }else{
+            echo json_encode("Id não existe");
+        }
+    }
+    else{
+        $sql = "SELECT id, nome, descricao, tecnologias, link_github, ano FROM projetos WHERE  status = 'publicado' ORDER BY ano DESC, id";
+        $projetos = $pdo->query($sql)->fetchAll();
+        echo json_encode($projetos);
+
+    }
+    
+
+
 ?>
--- api/tecnologias.php
+++ api/tecnologias.php
@@ -3,9 +3,22 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
 require __DIR__ . '/../conexao.php';
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-$sql = "SELECT id, nome, categoria, descricao, ano_criacao FROM tecnologias WHERE  status = 'ativo' ORDER BY categoria, id";
-$tecnologias = $pdo->query($sql)->fetchAll();
+ if($id){
+    $sql = $pdo->prepare("SELECT id, nome, categoria, descricao, ano_criacao FROM tecnologias WHERE id = :id AND status = 'ativo'");
+    $sql->execute(['id'=>$id]);
+    $tecnologias = $sql->fetch();
+    if($tecnologias){
+        echo json_encode($tecnologias);
+    }else{
+        echo json_encode("Id invalito");
+    }
+ }
+ else{
+    $sql = "SELECT id, nome, categoria, descricao, ano_criacao FROM tecnologias WHERE  status = 'ativo' ORDER BY categoria, id";
+    $tecnologias = $pdo->query($sql)->fetchAll();
+    echo json_encode($tecnologias);
+ }
 
-echo json_encode($tecnologias);
 ?>
