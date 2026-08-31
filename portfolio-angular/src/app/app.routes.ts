90ed38f794f1c99e9c353e4803629a9dd0e5dced
CaioZ-Stark <20241ctb0100031@estudantes.ifpr.edu.br>
2026-08-31 02:22:58 +0000

    Aula 19: area de gestao com CRUD completo (POST, PUT, DELETE)

--- portfolio-angular/src/app/app.routes.ts
+++ portfolio-angular/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Sobre } from './sobre/sobre';
 import { Projetos } from './projetos/projetos';
 import { Contat } from './contat/contat';
 import { Catalogo } from './catalogo/catalogo';
+import { Gestao } from './gestao/gestao';
 export const routes: Routes = [
     { path: '', component: Home},
     { path: 'sobre', component: Sobre},
     { path: 'projetos', component: Projetos},
     { path: 'contat', component: Contat},
     { path: 'catalogo', component: Catalogo},
+    { path: 'gestao', component: Gestao},
 ];
